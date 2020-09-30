01f2871911ae52097deb7857840d8d8c7f367859
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2020-09-30 15:01:56 +0900

    generate routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express();
+const bodyParser = require('body-parser') 
+let todoModel = require('./todo_schema')
 app.listen(3000, () => {
  console.log("Server started on port 3000")
 })
@@ -16,4 +18,59 @@ db.on('open', () => {
 });
 db.on('error', (error) => {
     console.log(error)
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+app.use(bodyParser.json());
+
+app.post('/todo/add', (req, res) => {
+  let newTodo = new todoModel;
+  newTodo.title = req.body.todo;
+  newTodo.completed = false;
+  newTodo.save((err) => {
+    if(err){
+      res.send("Error while adding Todo");
+    }else{
+      res.send("Todo added");
+    }
+})
+
+app.get('/todo/completed', (req, res) => {
+  todoModel.find({ completed: true }, (err, todos) => {
+    if (err) {
+      res.send("Error while fetching Todos");
+    } else {
+      res.json(todos)
+    }
+  })
+})
+
+app.get('/todo/uncompleted', (req, res) => {
+  todoModel.find({completed:false},(err, todos) => {
+    if(err){
+      res.send("Error while fetching Todos");
+    }else{
+      res.json(todos)
+    }
+  })
+})
+
+app.post('/todo/complete/:id',(req, res) => {
+  todoModel.findByIdAndUpdate(req.params.id, {completed: true},         (err, todo) =>{
+    if(!err){
+      res.send("Good Work");
+    }
+  })
+})
+
+app.delete('/todo/:id', (req, res) => {
+  let query = { _id: req.params.id }
+  todoModel.deleteOne(query, (err) => {
+    if(err){
+      res.send("Error while deleting todo")
+    }else{
+      res.send("Todo deleted")
+    }
+  })
 })
